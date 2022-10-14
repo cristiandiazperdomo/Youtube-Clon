cccda2aaacd62df1065764b43b9a7e7790874946
cristiandiazperdomo <danubiodiaz2003edf@gmail.com>
2022-10-14 17:49:17 -0300

    video container

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { Card } from './components/Card/Card';
 import { Header } from './components/Header/Header';
 import { Navigation } from './components/Navigation/Navigation';
+import { Video } from './components/Video/Video';
 
 ReactDOM.render(
   <React.StrictMode>
     <Header />
     <Navigation />
+    <Video />
   </React.StrictMode>,
   document.getElementById('root')
 );
