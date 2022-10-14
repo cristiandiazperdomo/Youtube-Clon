83ad84b4e42382602f3edbaf2c3e42a6c2dd51f4
cristiandiazperdomo <danubiodiaz2003edf@gmail.com>
2022-10-14 17:49:35 -0300

    video container

---
+++ src/components/Video/Video.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Card } from '../Card/Card';
+
+import './Video.css'
+export const Video = () => {
+	return (
+		<div className="Video-container">
+			<Card/>
+			<Card/>
+			<Card/>
+			<Card/>
+			<Card/>
+			<Card/>
+			<Card/>
+		</div>
+	)
+}
