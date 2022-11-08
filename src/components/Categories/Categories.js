02b045e3b0f7ad30297a7976edefced83af04c75
cristiandiazperdomo <danubiodiaz2003edf@gmail.com>
2022-11-08 14:13:01 -0300

    scroll main idea

--- src/components/Categories/Categories.js
+++ src/components/Categories/Categories.js
@@ -20,21 +20,30 @@ export const Categories = () => {
 				<ImCircleLeft />
 			</i>
 			<div className="Categories-container">
+				<Categorie categorie="Todo" />
+				<Categorie categorie="Mixes" />
 				<Categorie categorie="Música" />
+				<Categorie categorie="Podcast" />
+				<Categorie categorie="Videojuegos" />
+				<Categorie categorie="The Weeknd" />
+				<Categorie categorie="En directo" />
+				<Categorie categorie="Personajes" />
+				<Categorie categorie="Álbumes" />
+				<Categorie categorie="R&B" />
+				<Categorie categorie="Bossa Nova" />
 				<Categorie categorie="Música" />
+				<Categorie categorie="Podcast" />
+				<Categorie categorie="Videojuegos" />
+				<Categorie categorie="En directo" />
+				<Categorie categorie="Personajes" />
+				<Categorie categorie="Álbumes" />
+				<Categorie categorie="R&B" />
+				<Categorie categorie="Bossa Nova" />
+				<Categorie categorie="Mixes" />
 				<Categorie categorie="Música" />
-				<Categorie categorie="Música" />
-				<Categorie categorie="Música" />
-				<Categorie categorie="Música" />
-				<Categorie categorie="Música" />
-				<Categorie categorie="Música" />
-				<Categorie categorie="Música" />
-				<Categorie categorie="Música" />
-				<Categorie categorie="Música" />
-				<Categorie categorie="Música" />
-				<Categorie categorie="Música" />
-				<Categorie categorie="Música" />
-				<Categorie categorie="Música" />
+				<Categorie categorie="Podcast" />
+				<Categorie categorie="Videojuegos" />
+				<Categorie categorie="The Weeknd" />
 			</div>
 			<i onClick={scrollRight}>
 				<ImCircleRight />
